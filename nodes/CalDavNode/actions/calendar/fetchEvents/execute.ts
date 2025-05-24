1762c762b7b755afd5a8c5d745214dcec9ae794c
Oleg Tsvetkov <me@olegtsvetkov.com>
2025-05-24 22:26:26 +0300

    Simplify fetch events response

--- nodes/CalDavNode/actions/calendar/fetchEvents/execute.ts
+++ nodes/CalDavNode/actions/calendar/fetchEvents/execute.ts
@@ -3,7 +3,7 @@ import {createClient} from "../../../../../transport/davClient";
 import {DAVCalendar, DAVCalendarObject} from "tsdav";
 import { FormatDatetime } from "../../../methods";
 import { parseIcsCalendar } from "@ts-ics/schema-zod";
-import { IcsCalendar } from "ts-ics";
+import { IcsCalendar, IcsDateObject } from "ts-ics";
 
 export async function fetchObjects(this: IExecuteFunctions, index: number): Promise<INodeExecutionData[]> {
 	const client = await createClient(this, 'caldav');
@@ -30,19 +30,43 @@ export async function fetchObjects(this: IExecuteFunctions, index: number): Prom
 		}
 	});
 
+	// Create array for all events
+	const returnData: INodeExecutionData[] = [];
+
 	// Parse to events
-	const events = response.map(r => {
+	for (const r of response) {
 		const icsData = r.data as string;
 		const calendarParsed: IcsCalendar = parseIcsCalendar(icsData);
 
-		return {
-			url: r.url,
-			etag: r.etag as string,
-			event: calendarParsed.events
-		};
-	});
+		if (!calendarParsed.events) {
+			continue;
+		}
 
-	return this.helpers.returnJsonArray({
-		events: events,
-	});
+		// Transform dates in events to IcsDateObject
+		const transformedEvents = calendarParsed.events.map(event => {
+			const transformedEvent = { ...event } as Record<string, any>;
+			
+			// Transform all fields with dates to IcsDateObject
+			Object.keys(transformedEvent).forEach(key => {
+				const value = transformedEvent[key];
+				if (value && typeof value === 'object' && 'type' in value && (value.type === 'DATE-TIME' || value.type === 'DATE')) {
+					transformedEvent[key] = (value as IcsDateObject).date;
+				}
+			});
+
+			return {
+				json: {
+					...transformedEvent,
+					_handle: {
+						calendarUrl: r.url,
+						etag: r.etag as string
+					}
+				}
+			};
+		});
+
+		returnData.push(...transformedEvents);
+	}
+
+	return this.helpers.returnJsonArray(returnData);
 }
